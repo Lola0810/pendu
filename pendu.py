eca285d9737fdf7bc66973e0d78538707294ba85
Papipomme <67956687+Papipomme@users.noreply.github.com>
2021-03-26 20:32:16 +0100

    feat: Add char images to the game
    Signed-off-by: Papipomme <papipomme@protonmail.com>

--- pendu.py
+++ pendu.py
@@ -52,7 +52,7 @@ def boucle_pendu():
         if lettre in lettres_trouve:
             lettres_mot.append(lettre)
 
-    return (len(lettres_mot) == len(mot)) or erreurs == 6
+    return (len(lettres_mot) == len(mot)) or erreurs == 8
 
 
 def demarrer():
@@ -65,7 +65,7 @@ def demarrer():
         verifier_lettre(lettre)
         afficher_pendu()
 
-    if erreurs == 6:
+    if erreurs == 8:
         print(f'Désolé vous avez perdu, le mot était {mot}')
     else:
         print('Bravo vous avez gagné')
